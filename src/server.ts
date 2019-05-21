7b1de0e79226381e51e4c10cc86eb8e5696d96eb
iburistu <zachary.linkletter@maine.edu>
2019-05-21 18:49:48 -0400

    Removing websockets and serialport

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ import * as express from 'express';
 import * as http from 'http';
 import * as WebSocket from 'ws';
 import * as sqlite3 from 'sqlite3'
-import * as SerialPort from 'serialport'
 
 var Readline = require('@serialport/parser-readline');
+
+let data: any[];
 
 const app = express();
 
@@ -13,7 +14,7 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 // Database connection
-let db = new sqlite3.Database(':memory:', (err) => {
+let db = new sqlite3.Database('../datalogger/database/database.db', (err) => {
     if (err) {
         // Cannot open database
         console.log('Error connecting to database!');
@@ -24,17 +25,6 @@ let db = new sqlite3.Database(':memory:', (err) => {
     }
 });
 
-// Serial Port
-const port = new SerialPort('/dev/ttyAMA0', {
-    baudRate: 38400
-}, (err) => {
-    if (err) {
-        return console.log(`${err.message}`);
-    }
-});
-// Parser
-const parser = port.pipe(new Readline({ delimiter: '\r\n'}));
-
 // Websocket
 wss.on('connection', (ws: WebSocket) => {
     ws.send('Connected sucessfully!');
@@ -43,13 +33,23 @@ wss.on('connection', (ws: WebSocket) => {
     })
 });
 
+// SQL query
+let sql = `SELECT * FROM data LIMIT 1440 OFFSET (SELECT COUNT(*) FROM data)-1440;`;
+
 // Update loop; updates once a minute
 setInterval(() => {
-    wss.clients.forEach((ws: WebSocket) => {
-        ws.send("Testing");
-    })
-}, 60000);
+    db.each(sql, [], (err: any, rows: any) => {
+	if (err) {
+	    throw err;
+	}
+	console.log(rows);
+    });
+    //    wss.clients.forEach((ws: WebSocket) => {
+    //    ws.send("Testing");
+    //})
+}, 10000);
 
 server.listen(4000, () => {
     console.log(`Server started on port 4000`);
+
 })
