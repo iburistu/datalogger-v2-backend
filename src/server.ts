3739f445dc3c86ef8140c4c68156814ef90b3ce6
iburistu <zachary.linkletter@maine.edu>
2019-05-24 16:13:54 -0400

    Running on RPi

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import * as http from 'http';
-import * as sqlite3 from 'sqlite3'
+import * as sqlite3 from 'sqlite3';
 
 const app = express();
 
@@ -18,15 +18,24 @@ let db = new sqlite3.Database('../datalogger/database/database.db', (err) => {
     }
 });
 
-app.get("/api/temperature", function (req, res) {
-    let sql = `SELECT ? FROM data LIMIT 1440 OFFSET (SELECT COUNT(*) FROM data)-1440;`;
-    db.each(sql, ["hrtemp"], (err, rows) => {
+app.get("/api/:parameter", function (req, res) {
+    let param = req.params.parameter;
+    let sql = `SELECT "${param}" AS "y", "t" FROM data LIMIT 1440 OFFSET (SELECT COUNT(*) FROM data)-1440`;
+    db.all(sql, [], (err, rows) => {
         if (err) {
             throw err;
         }
         res.send(JSON.stringify(rows));
     })
-})
+});
+
+app.use(express.static('../datalogger-v2-react/build'));
+
+app.get("/*", function (req, res) {
+    res.sendFile('/home/pi/Documents/datalogger-v2-react/build/index.html', (err) => { 
+	if(err) throw err;
+    });
+});
 
 server.listen(4000, () => {
     console.log(`Server started on port 4000`);
